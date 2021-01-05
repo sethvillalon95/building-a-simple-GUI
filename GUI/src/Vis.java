050bb049fd6fa49e012475cce47209c68cf1a344
seth <sethvillalon95@gmail.com>
2021-01-05 12:21:23 +0800

    Add a US flag

--- GUI/src/Vis.java
+++ GUI/src/Vis.java
@@ -27,6 +27,53 @@ public class Vis extends JPanel {
         //render visualization
         g.setColor(Color.BLACK);
         g.drawString("Your content here", 10, 20);
+        int w = getWidth(); 
+		int h = getHeight(); 
+		int numStripes = 13;
+		int stripeHeight = h / numStripes;
+		int stripeHeight1 = h / numStripes;
+		int stripeWidth = w;
+		int blueBoxWidth = 2*w/5;
+		int blueBoxHeight = 7*stripeHeight;
+		int numRows = 6;
+		int numCols = 8;
+		int starWidth = blueBoxWidth/numCols;
+		int starHeight = blueBoxHeight/numRows;
+		
+	
+		
+		//background color 
+		g.setColor(Color.RED);
+		g.fillRect(0,0,w,h);
+		
+		
+		// remove stripes?
+		for (int i = 0; i < 6; i++){
+			g.setColor(Color.WHITE);
+			g.fillRect(0,stripeHeight1,stripeWidth,stripeHeight);
+			stripeHeight1 += stripeHeight*2;
+		}
+		
+		// Box 
+		g.setColor(Color.BLUE);
+		g.fillRect(0,0,blueBoxWidth,blueBoxHeight);
+		
+		//remove stripes?
+		// Stars 
+		int y = 0 ;
+		for ( int i = 0; i < numRows; i++){
+			int x = 0 ;
+			for (int j = 0; j < numCols; j++){
+				g.setColor(Color.WHITE);
+				g.fillOval( x, y,starWidth, starHeight);
+				x +=starWidth;
+				
+			}
+			y +=starHeight;
+			
+		}
+		
+		
     }
 
 }
